3313e527189e2771be252a2edbca4c119acf064e
nova1147 <tetra.00aero@icloud.com>
2025-09-28 09:18:22 +0100

    Enhance 师生互动工具

---
+++ TeacherStudentInteraction_0928_1618_pob.cs
@@ -0,0 +1,12 @@
+// 代码生成时间: 2025-09-28 16:18:21
+using System;
+# NOTE: 重要实现细节
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+// 定义师生互动工具的控制器
+[ApiController]
+[Route("api/[controller]/[action]
